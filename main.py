0b454fa97ab8083b58445e7cd7ee276940cf4493
mayahawk14 <maya.hawk14@gmail.com>
2026-05-14 14:26:09 -0500

    Remove unnecesary comments

--- main.py
+++ main.py
@@ -1,30 +1,7 @@
 import xml.etree.ElementTree as ET
 
-""""
-xml_data = "/Users/mayahawkins/repos/Script_Splitter/rot.xml"
 
-# Parse the XML data
-root = ET.parse(xml_data).getroot()
-
-# Iterate over elements
-for parent in root:
-    print(f"Parent tag: {parent.tag}")
-    for child in parent:
-        print(f"Child tag: {child.tag}")
-        for grandchild in child:
-            print(f"Grandchild tag: {grandchild.tag}, Attribute: {grandchild.attrib}")
-
-# Access elements
-product = root.find(".//product")
-name = product.find(".//name").text
-price = product.find(".//price").text
-rate = product.find(".//rate").text 
-description = product.find(".//description").text 
-
-print(f"Name: {name}, Price: {price}, Rate: {rate}, Description: {description}")
-
-"""
-xml_data = "/Users/mayahawkins/repos/Script_Splitter/DearMomc.xml"
+xml_data = "/Users/mayahawkins/repos/Script_Splitter/DearMom.xml"
 
 # Parse the XML data
 tree = ET.parse(xml_data)
@@ -111,7 +88,3 @@ def options():
 options()
 
 
-
-#print(f"Name: {name}, Price: {price}, Rate: {rate}, Description: {description}")
-#print(f"Title: {titlePage}")
-#print(f"Paragraph Type: {paragraph}, Text: {text}")
